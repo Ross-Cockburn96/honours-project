53d65ca5ba4f35425da6c150bf43ddec3538bf60
rossco906 <s1424268@ed.ac.uk>
2019-11-08 17:51:38 +0000

    Revert "test"
    This reverts commit af0870f57de5faf7d3c55d2962cf028947b13b31.

--- main.py
+++
@@ -1,9 +0,0 @@
-
-import problemGenerator.Generator as Generator
-import random
-import numpy as np
-
-if __name__ == "__main__":
-    
-    generator = Generator.Generator(nodeDensity=.20)
-    generator.generateNodes()
--- problemGenerator/Node.py
+++
@@ -1,7 +0,0 @@
-class Node:
-    def __init__(self, xCoord, yCoord):
-        self.xCoord = xCoord
-        self.yCoord = yCoord
-
-    def str(self):
-        return (self.xCoord, self.yCoord)    
