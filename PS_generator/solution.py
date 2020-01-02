dc8d1ee19e82f2df3857640195a93466cce3e6b1
Ross <ross.s.cockburn@gmail.com>
2020-01-02 15:49:54 +0000

    added solution object and solutions are now randomly generated

---
+++ PS_generator/solution.py
@@ -0,0 +1,54 @@
+import random
+import parameters
+import math
+from drone import Drone 
+from trip import Trip
+from delivery import Delivery
+
+
+
+class Solution:
+    customers = []
+    drones = []
+
+    def __init__(self, customers): 
+        self.customers = customers 
+
+    def generate(self):
+        numOfTrips = random.randint(1,parameters.customers)
+        numOfDrones = random.randint(1, numOfTrips)
+        trips = [Trip(self.customers.pop(random.randrange(len(self.customers)))) for _ in range(numOfTrips)] #ensure that each trip has at least one delviery 
+        
+
+        for customer in self.customers: 
+            trip = trips[random.randrange(numOfTrips)]
+            trip.addDelivery(customer)
+
+        self.drones = [Drone(trips.pop(random.randrange(len(trips)))) for _ in range(numOfDrones)] #ensure that each drone has at least one trip
+        
+        for trip in trips: 
+            drone = self.drones[random.randrange(numOfDrones)]
+            drone.addTrip(trip)
+
+        for drone in self.drones: 
+            minimumDeliveryTime = 100 
+            for trip in drone.trips:
+                for delivery in trip.deliveries:
+                    minimumDeliveryTime = math.floor(abs(random.random() - random.random()) *  (1 + 10000 - minimumDeliveryTime) + minimumDeliveryTime )
+                    delivery.time = minimumDeliveryTime
+
+        for idx, drone in enumerate(self.drones): 
+            print(f"Drone No. {idx}: {drone}")
+    
+    def __repr__(self):
+        outputElements = []
+        for drone in self.drones: 
+            outputElements.append(len(drone.trips))
+            for trip in drone.trips:
+                outputElements.append(len(trip.deliveries))
+                for delivery in trip.deliveries: 
+                    outputElements.append(delivery.node)
+                    outputElements.append(delivery.time)
+            outputElements.append("\n")
+        return ("".join([(str(x) + ", " if x != "\n" else "\n") for x in outputElements])) 
+
