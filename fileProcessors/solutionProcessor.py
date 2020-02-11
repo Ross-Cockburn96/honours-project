a3c959590a6325e78b8fdfe1d33e76f1aa873379
rossco906 <s1424268@ed.ac.uk>
2020-02-11 18:01:06 +0000

    added comments

--- fileProcessors/solutionProcessor.py
+++ fileProcessors/solutionProcessor.py
@@ -14,22 +14,25 @@ def process(filePath):
     tripCountIdx = 0 
     outputString = []
     finished = False
+    
+    #iterates through drones 
     while not finished: 
         if tripCountIdx < len(elements):
             droneTrips = int(elements[tripCountIdx])
-            outputString.append(str(droneTrips))
+            outputString.append(str(droneTrips)) #adds number of trips the drone has to string 
 
             tripCountIdx += 1
 
+            #iterates through deliveries in trip
             while droneTrips > 0: 
                 tripsForDrone += 1 
                 deliveryCount = int(elements[tripCountIdx])
-                outputString.append(str(deliveryCount))
+                outputString.append(str(deliveryCount)) #adds number of deliveries in trip to string 
                 tripCountIdx += (deliveryCount * 2) + 1
                 droneTrips -= 1
         
         else:
             finished = True 
             continue
-        outputString.append('\n')
+        outputString.append('\n') #add new line per drone 
     return "".join([(str(x) + ", " if x != "\n" else "\n") for x in outputString])
