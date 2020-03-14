962376f6973fc963cddbf73382215c7e9f528fc9
Ross <ross.s.cockburn@gmail.com>
2020-03-14 20:39:52 +0000

    removed print statements

--- finalProject/algorithms/genetic.py
+++ finalProject/algorithms/genetic.py
@@ -31,7 +31,6 @@ nodes = []
 
 nodes, packages = buildNodes(problemElements)
 
-print(f"length of packages is {len(packages)}")
 params["numGenes"] = len(packages)
 #genetic algorithm parameters
 popsize = 1
@@ -73,7 +72,6 @@ def decoder(individual):
         #if the new delivery made the trip invalid then remove it from trip and form the trip object
         if (cargoTracker > params["cargoSlotNum"]) or (weightTracker > params["cargoWeightLimit"]):
             
-            print(idx)
             #reset trackers
             cargoTracker = 1
             weightTracker = package.weight
@@ -86,50 +84,32 @@ def decoder(individual):
             trip = Trip(*droneActions)
             
             #action that was not taken on this trip will be part of the next trip
-            print(f"drone actions before delete is {droneActions}")
             droneActions = [newDelivery]
         
             tripDistance = Node.distanceCalc(*[action.node for action in trip.actions])
             #drone can't deliver this trip i
             if (drone.distanceLeft < tripDistance): 
                 drones.append(drone)
-                print(f"forming drone index {idx}")
-                print(f"actions on next trip are {droneActions}")
-                for trip2 in drone.trips:
-                    print(f"forming with {trip2}")
                 #start building new drone
                 
                 drone = Drone(trip)
             else:
                 drone.trips.append(trip)
-                print(f"added trip {idx} {len(trip.actions)}")
                 drone.distanceLeft -= tripDistance
         if idx == params["numGenes"] - 1:
-            print(f"final")
             droneActions.insert(0, AtDepot())
             droneActions.append(AtDepot())
             trip = Trip(*droneActions)
             tripDistance = Node.distanceCalc(*[action.node for action in trip.actions])
             #if drone can't deliver the trip add the drone and create and add new drone to finish decoding
             if (drone.distanceLeft < tripDistance): 
-                print(f"new drone")
                 drones.append(drone)
                 drones.append(Drone(trip))
             else:
-                print(f"old drone")
                 drone.trips.append(trip)
                 drones.append(drone)
 
     counter = 0
-    print(f"chromosome is {individual.chromosome}")
-    for drone in drones:
-        for trip in drone.trips:
-            for action in trip.actions: 
-                if "Delivery" in str(type(action)):
-                    print(f"{action.package.id}  {nodes[action.package.destination]}")
-                    
-                    counter += 1
-    print(counter)
     elements = phenotype(drones)
     return elements
 
