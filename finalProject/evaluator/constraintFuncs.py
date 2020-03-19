1f62255d8331fd2acc28de3bfe07703dce39d1e3
rossco906 <s1424268@ed.ac.uk>
2020-03-19 10:59:07 +0000

    refactored constraint functions

---
+++ finalProject/evaluator/constraintFuncs.py
@@ -0,0 +1,95 @@
+from generatorObjects.node import Depot, Node
+
+
+def countUniquePackagesDelivered(drones):
+    packages = [] 
+
+    #create a list of all packages delivered in the solution
+    for drone in drones:
+        for action in drone.getAllActions():
+            if "Delivery" in str(type(action)):
+                packages.append(action.package.id)
+    return len(set(packages))
+
+def checkCustomerDemandsSatisfied(drones, packages):
+    packageDemandDic = dict(zip([pkg.id for pkg in packages], [pkg.destination for pkg in packages]))
+    packagesDeliveredCorrectly = 0
+    for drone in drones:
+        for trip in drone.trips:
+            for action in trip.actions:
+                if "Delivery" in str(type(action)):
+                    if action.node.id == packageDemandDic[action.package.id]:
+                        packagesDeliveredCorrectly += 1
+
+    return packagesDeliveredCorrectly
+
+
+def countBatteriesUsed(drones):
+    batteries = []
+    #create a list of all batteries used in the solution
+    batteries.extend(Depot.batteriesHeld)
+    for drone in drones:
+        batteries.append(drone.battery)
+        for action in drone.getAllActions():
+            if "ChangeBattery" in str(type(action)):
+                print(f"change battery action: {action}, {type(action)}")
+                batteries.append(action.batterySelected)
+    batteries.sort(key=lambda x : x.id)
+    return len(set(batteries))
+
+def countDroneChargeDepletion(drones): 
+    numberOfDepletions = 0
+    for drone in drones: 
+        for trip in drone.trips: 
+            for action in trip.actions[1:]: 
+                if "Delivery" in str(type(action)) or "AtDepot" in str(type(action)):
+                    distanceTraveled = Node.distanceFinder(action.node, action.prevAction.node)
+                    drone.battery.batteryDistance -= distanceTraveled
+                else:
+                    drone.battery = action.batterySelected
+                    #may need to calculate the charge of the battery selected
+                if drone.battery.batteryDistance < 0: 
+                    numberOfDepletions += 1
+                    break
+            else:
+                continue 
+            break
+    return numberOfDepletions
+
+def droneCargoCount(drones):
+    numberOfTripsWithTooManyPackages = 0
+    tripCount = 0
+    for drone in drones:
+        for trip in drone.trips:
+            tripCount += 1
+            packageCount = 0
+            for action in trip.actions:
+                if "Delivery" in str(type(action)):
+                    packageCount += 1
+            if packageCount > 5: 
+                numberOfTripsWithTooManyPackages += 1
+    return numberOfTripsWithTooManyPackages, tripCount
+
+def droneWeightCount(drones):
+    numberOfTripswithTooHeavyPackages = 0 
+    for drone in drones:
+        for trip in drone.trips:
+            weightTotal = 0
+            for action in trip.actions:
+                if "Delivery" in str(type(action)):
+                    weightTotal += action.package.weight
+            if weightTotal > 30: 
+                numberOfTripswithTooHeavyPackages += 1
+    return numberOfTripswithTooHeavyPackages
+
+def checkStartAndFinishPositions(drones): 
+    tripsNotStartingAtDepot = 0 
+    tripsNotFinishingAtDepot = 0 
+    for drone in drones:
+        for trip in drone.trips: 
+            if trip.actions[0].node.getCoords() != (0,0):
+                tripsNotStartingAtDepot += 1
+            if trip.actions[-1].node.getCoords() != (0,0):
+                tripsNotFinishingAtDepot += 1
+    
+    return tripsNotFinishingAtDepot, tripsNotStartingAtDepot
--- finalProject/evaluator/evaluator.py
+++ finalProject/evaluator/evaluator.py
@@ -3,6 +3,7 @@ import argparse
 from fileParsers.nodeBuilder import buildNodes
 from fileParsers.objectBuilder import buildObjects
 from generatorObjects.node import Depot, Node
+from .constraintFuncs import * 
 #from context import generatorObjects
 
 parser = argparse.ArgumentParser()
@@ -62,99 +63,6 @@ with open(solution) as file:
     solutionElements = solutionData.split(",")
     solutionElements = [int(e) for e in solutionElements]
     drones = buildObjects(solutionElements, numberOfCustomers, nodes, packages)
-    
-
-def countUniquePackagesDelivered():
-    packages = [] 
-
-    #create a list of all packages delivered in the solution
-    for drone in drones:
-        for action in drone.getAllActions():
-            if "Delivery" in str(type(action)):
-                packages.append(action.package.id)
-    return len(set(packages))
-
-def checkCustomerDemandsSatisfied():
-    packageDemandDic = dict(zip([pkg.id for pkg in packages], [pkg.destination for pkg in packages]))
-    packagesDeliveredCorrectly = 0
-    for drone in drones:
-        for trip in drone.trips:
-            for action in trip.actions:
-                if "Delivery" in str(type(action)):
-                    if action.node.id == packageDemandDic[action.package.id]:
-                        packagesDeliveredCorrectly += 1
-    
-    return packagesDeliveredCorrectly
-
-def countBatteriesUsed():
-    batteries = []
-    #create a list of all batteries used in the solution
-    batteries.extend(Depot.batteriesHeld)
-    for drone in drones:
-        batteries.append(drone.battery)
-        for action in drone.getAllActions():
-            if "ChangeBattery" in str(type(action)):
-                print(f"change battery action: {action}, {type(action)}")
-                batteries.append(action.batterySelected)
-    batteries.sort(key=lambda x : x.id)
-    return len(set(batteries))
-
-def countDroneChargeDepletion(): 
-    numberOfDepletions = 0
-    for drone in drones: 
-        for trip in drone.trips: 
-            for action in trip.actions[1:]: 
-                if "Delivery" in str(type(action)) or "AtDepot" in str(type(action)):
-                    distanceTraveled = Node.distanceFinder(action.node, action.prevAction.node)
-                    drone.battery.batteryDistance -= distanceTraveled
-                else:
-                    drone.battery = action.batterySelected
-                    #may need to calculate the charge of the battery selected
-                if drone.battery.batteryDistance < 0: 
-                    numberOfDepletions += 1
-                    break
-            else:
-                continue 
-            break
-    return numberOfDepletions
-
-def droneCargoCount():
-    numberOfTripsWithTooManyPackages = 0
-    tripCount = 0
-    for drone in drones:
-        for trip in drone.trips:
-            tripCount += 1
-            packageCount = 0
-            for action in trip.actions:
-                if "Delivery" in str(type(action)):
-                    packageCount += 1
-            if packageCount > 5: 
-                numberOfTripsWithTooManyPackages += 1
-    return numberOfTripsWithTooManyPackages, tripCount
-
-def droneWeightCount():
-    numberOfTripswithTooHeavyPackages = 0 
-    for drone in drones:
-        for trip in drone.trips:
-            weightTotal = 0
-            for action in trip.actions:
-                if "Delivery" in str(type(action)):
-                    weightTotal += action.package.weight
-            if weightTotal > 30: 
-                numberOfTripswithTooHeavyPackages += 1
-    return numberOfTripswithTooHeavyPackages
-
-def checkStartAndFinishPositions(): 
-    tripsNotStartingAtDepot = 0 
-    tripsNotFinishingAtDepot = 0 
-    for drone in drones:
-        for trip in drone.trips: 
-            if trip.actions[0].node.getCoords() != (0,0):
-                tripsNotStartingAtDepot += 1
-            if trip.actions[-1].node.getCoords() != (0,0):
-                tripsNotFinishingAtDepot += 1
-    
-    return tripsNotFinishingAtDepot, tripsNotStartingAtDepot
             
 def initialiseObjectiveConstants():
     global maxDrones, maxDistanceTraveled, maxBatteries, dronesUsedMultiplier, batteriesUsedMultiplier
@@ -192,49 +100,49 @@ with open(outputLocation, "w") as file:
         result = "FAIL"
     file.write(f"Drones used in solution => {solutionElements[0]}: {result}\n")
     
-    packagesDelivered = countUniquePackagesDelivered()
+    packagesDelivered = countUniquePackagesDelivered(drones)
     if packagesDelivered == numberOfPackages:
         result == "PASS"
     else:
         result = "FAIL"
     file.write(f"Packages scheduled for delivery by solution => {packagesDelivered}/{numberOfPackages}: {result}\n")
 
-    correctlyDeliveredPackages = checkCustomerDemandsSatisfied()
+    correctlyDeliveredPackages = checkCustomerDemandsSatisfied(drones, packages)
     if correctlyDeliveredPackages == numberOfPackages:
         result = "PASS"
     else:
         result = "FAIL"
     file.write(f"Packages delivered to correct customer => {correctlyDeliveredPackages}/{numberOfPackages}: {result}\n")
     
-    batteriesUsed = countBatteriesUsed()
+    batteriesUsed = countBatteriesUsed(drones)
     if batteriesUsed > maxBatteriesAvailable:
         result = "FAIL"
     else:
         result = "PASS"
     file.write(f"Number of batteries used by solution => {batteriesUsed}, maximum available is {maxBatteriesAvailable}: {result}\n")
 
-    batteriesOutOfCharge = countDroneChargeDepletion()
+    batteriesOutOfCharge = countDroneChargeDepletion(drones)
     if batteriesOutOfCharge > 0: 
         result = "FAIL"
     else:
         result = "PASS"
     file.write(f"Number of drones that ran out of power in solution => {batteriesOutOfCharge}/{len(drones)}: {result}\n")
 
-    overloadedTrips, numOfTrips = droneCargoCount()
+    overloadedTrips, numOfTrips = droneCargoCount(drones)
     if overloadedTrips > 0: 
         result = "FAIL" 
     else: 
         result = "PASS" 
     file.write(f"Number of trips that overloaded the drone cargo limit => {overloadedTrips}/{numOfTrips}: {result}\n")
 
-    overweightedTrips = droneWeightCount()
+    overweightedTrips = droneWeightCount(drones)
     if overweightedTrips > 0:
         result = "FAIL"
     else:
         result = "PASS"
     file.write(f"Number of trips that contain a cargo that is too heavy for their drone => {overweightedTrips}/{numOfTrips}: {result}\n")
     
-    notFinishing, notStarting = checkStartAndFinishPositions()
+    notFinishing, notStarting = checkStartAndFinishPositions(drones)
     if notStarting > 0: 
         result = "FAIL" 
     else:
